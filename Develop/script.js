acba5c02dcc2e6d9456bbfec1bd38563a9ffa5a9
Chelsey Craig <chelsey.craig7321@gmail.com>
2020-06-15 23:55:19 -0400

    Updated Files

--- Develop/script.js
+++ Develop/script.js
@@ -1,34 +1,35 @@
+// Function that will be used to store the data that's input by user
 function storeData(data, name){
+    // Moving items to local storage after they've been saved
     localStorage.setItem(name, JSON.stringify(data));
 }
 
+// Function that will take data out of local storage and parse it
 function retrieveData(name){
     return JSON.parse(localStorage.getItem(name))
 }
 
-console.log(retrieveData("calendar"))
-
+// Function to combine all text, time, and item data
 function getAllTextData(t){
     var dataArray = []
     var $textareas = $(".tasks")
     $.each($textareas, function(index, value){
-        console.log(t, $(value).data("time"))
+        // Setting conditions for function to follow
         if(t == $(value).data("time")){
-            console.log("they match!")
             var text = $(value).val()
             var time = $(value).data("time")
+            // This will loop through the time arry is conditions are met
             for(i = 0; i < timeArray.length; i++){
                 var item = timeArray[i]
-                console.log(text, time, item)
+                // If statement is true, set to text
                 if(item.time == time){
 
                     item.text = text
                 }
             }
         }
-        console.log($(value).text())
     })
-    console.log(timeArray)
+    // Storing new data to the time array
     storeData(timeArray, "timeArray")
 }
 
@@ -66,50 +67,59 @@ defaultTimeArray = [
 ]
 // If not time array, set it to default TimeArray
 var timeArray = retrieveData("timeArray") || defaultTimeArray
-console.log(timeArray)
 
 // Will be hours on scheduler
 var timeBlock = $("#timeBlock")
+// Using moment JS to format date from military time to standard time
 var displayTime = moment().format("h a")
 console.log(moment().format("dddd MMMM DD YYYY"))
+// Retrieving date from the current day
 $("#currentDay").text(moment().format("dddd MMMM DD YYYY"))
 
+// Using moment JS to retrieve current time for planning blocks
 var currentHour = moment().format("HH")
 
 populateTimeBlock()
-
+// This will loop through length of time array to find current time
 function populateTimeBlock(){
     for(i = 0; i < timeArray.length; i++){
         var calendarHour = moment(timeArray[i].time, "HH");
         var calendarTime = calendarHour.format("h a")
         console.log(calendarTime)
-        console.log(calendarHour.isBefore(moment(currentHour, "HH")))
 
         var isBefore = calendarHour.isBefore(moment(currentHour, "HH"))
         var isAfter = calendarHour.isAfter(moment(currentHour, "HH"))
         var colorClass
+        // If time is before current time, they will get characteristics of "past" color class 
         if (isBefore){
             colorClass = "past"
+        // If time is after current time, they will get characteristics of "future" color class 
         }else if(isAfter){
             colorClass = "future"
         }
+        // Time during current hour will get characteristics of "present" color class 
         else {
             colorClass = "present"
         }
         createPlannerRow(timeArray[i], colorClass)
     }
 
+    // This defines each section of the planner row by column
     function createPlannerRow(timeObj, colorClass){ // t is time
-        var time = $("<div></div>").addClass("time").text(timeObj.time)
-        var tasks = $("<textarea></textarea>").addClass("tasks").addClass(colorClass).text(timeObj.text).data("time", timeObj.time)
-        var saveBtn = $("<div></div>").addClass("saveBtn").text("save").data("time", timeObj.time)
-
-        var plannerRow = $("<div></div>").addClass("plannerRow row").append(time, tasks, saveBtn).data("time", timeObj.time)
+        var calendarTime = moment(timeObj.time, "HH").format("hh a");
+        // Defines how to obtain the time, task, save button, and planner row elements
+        var time = $("<div></div>").addClass("time col-1").text(calendarTime)
+        var tasks = $("<textarea></textarea>").addClass("tasks col-10 " + colorClass).text(timeObj.text).data("time", timeObj.time)
+        var saveBtn = $("<div></div>").addClass("saveBtn col-1 ").data("time", timeObj.time).append("<i class='fa fa-save'></i>"); //appends save icon to save button area
+        var plannerRow = $("<div></div>").addClass("plannerRow row").append(time, tasks, saveBtn).data("time", timeObj.time) // appends data to planner row
+        // Appends planner row to the time block
         timeBlock.append(plannerRow)
+
     }
 
 }
 
+// 
 getAllTextData()
 $(".saveBtn").on("click", function(event){
     var el = $(this)
@@ -118,17 +128,3 @@ $(".saveBtn").on("click", function(event){
     getAllTextData(theTime)
     console.log("click")
 })
-//                        
-
-
-// Must be able to click to type in an event
-
-
-
-
-// Must be able to save event into local storage by clicking on right hand icon
-
-
-
-
-// Item must stay on schedule after page is refreshed
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Daily-Planner
+
+Plan your work day in a snap!
+
+___
+
+## Description
+
+The daily planner is a helpful tool that will allow the user to write, save, and view his/her plans for the work day.  The main body changes colors depending on the time of day. It is an interactive display that highlights the days events.
+___
+
+## How It Works
+
+The daily planner is arranged with rows and columns that the provide the user with a number of options to chose from. In the main body of the planner, the hour corresponding to the current time is displayed.  As the time changes, the hour rows change colors to keep the user aware of their current tasks for that hour, the past tasks that have already happened in that day, and what is to come until end of the work day.
+
+The user can interact with the planner by adding in tasks at the needed time, then clicking the save button on the right side of the screen.  By doing this, the users tasks get saved into local storage where they will stay until the user deletes the task.
+
+The functionality of the planner is based around the usage of Moment JS that is used to not only indicate the hour of the day, but to also set the date in the header of the page. Below is a visual representation of the daily planner. During the hours of 9 AM - 5 PM, the calendar displays red for the current hour and green for anytime in the future. For this example, the blocks are grey to show that the work has ended.
+
+![IMAGE of END OF DAILY PLANNER](/Users/chelseycraig/Desktop/code/Homework/Daily-Planner/README.md)
+___
+
+## Technologies
+
+- HTML
+- CSS
+- Moment JS
+- Bootstrap
+- Bootswatch
+- Javascript
+- Visual Studio Code
+
+___
+
+### License
+
+Copyright 2020 CHELSEY CRAIG
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
